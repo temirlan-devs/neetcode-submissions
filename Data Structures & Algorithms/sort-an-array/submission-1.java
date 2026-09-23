1a3fa978e480bb68d6fc116f9a5a2930069cebf9
temirlan-devs <temirlan.yermekov@outlook.com>
2026-09-23 14:12:51 +0100

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    int[] nums;
+
+    public int[] sortArray(int[] nums) {
+        this.nums = nums;
+        mergeSort(0, nums.length - 1);
+
+        return nums;
+    }
+
+    public void mergeSort(int l, int r) {
+        if (l >= r) return;
+        int m = (l + r) / 2;
+
+        mergeSort(l, m);
+        mergeSort(m + 1, r);
+        merge(l, m, r);
+    }
+
+    public void merge(int l, int m, int r) {
+        int i = l;
+        int j = m + 1;
+
+        List<Integer> temp = new ArrayList<>();
+
+        while (i <= m && j <= r) {
+            if (nums[i] <= nums[j]) {
+                temp.add(nums[i]);
+                i++;
+            } else {
+                temp.add(nums[j]);
+                j++;
+            }
+        }
+
+        while (i <= m) {
+            temp.add(nums[i]);
+            i++;
+        }
+        while (j <= r) {
+            temp.add(nums[j]);
+            j++;
+        }
+
+        for (i = l; i <= r; i++) {
+            nums[i] = temp.get(i - l);
+        }
+    }
+}
