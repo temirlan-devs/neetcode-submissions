86aa4497d7b29bc2eaa486a9bf1e11fba5e82b23
temirlan-devs <temirlan.yermekov@outlook.com>
2026-09-24 10:37:31 +0100

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    int[] nums;
+
+    public void sortColors(int[] nums) {
+        this.nums = nums;
+        int i = 0, l = 0, r = nums.length - 1;
+
+        while (i <= r) {
+            if (nums[i] == 0) {
+                swap(l, i);
+                l++;
+            } 
+            else if (nums[i] == 2) {
+                swap(i, r);
+                r--;
+                i--;
+            }
+            i++;
+        }
+    }
+
+    public void swap(int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
