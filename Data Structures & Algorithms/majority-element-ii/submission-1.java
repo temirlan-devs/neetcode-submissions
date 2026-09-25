3aa37b90eec1e7e0d61bef1d557709b0ca72cb31
temirlan-devs <temirlan.yermekov@outlook.com>
2026-09-25 11:43:29 +0100

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            if (map.size() <= 2) continue;
+
+            Map<Integer, Integer> new_map = new HashMap<>(); 
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if (entry.getValue() > 1) {
+                    new_map.put(entry.getKey(), entry.getValue() - 1);
+                }
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int key : map.keySet()) {
+            int counter = 0;
+            for (int num : nums) {
+                if (key == num) counter++;
+            }
+            if (counter > (nums.length / 3)) res.add(key);
+        }
+
+        return res;
+
+    }
+}
